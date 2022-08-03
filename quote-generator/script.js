386986abebeb2585f904f8e1894af045a58575fd
musarri <msaricicek_99@hotmail.com>
2022-08-03 21:26:44 +0300

    DOM manipulated

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,8 +1,26 @@
 let apiQuotes = []
 
+const container = document.querySelector(".container")
+const text = document.querySelector(".text")
+const author = document.querySelector(".author")
+const twitter = document.querySelector(".twitter")
+const newQuoteBtn = document.querySelector(".new-quote")
+
+
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote);
+    console.log(quote); 
+
+    if(!quote.author){
+        author.textContent = "Anonym"
+    }
+
+    if(quote.text.length > 120){
+        text.classList.add("small-size")
+    }
+
+    text.textContent = quote.text
+    author.textContent = quote.author
 }
 
 async function getQuotes(){
