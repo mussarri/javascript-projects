eca5c1c8f2053ac0ba28de02000efae213e8dc3e
musarri <msaricicek_99@hotmail.com>
2022-08-03 19:17:39 +0300

    API fetched

---
+++ quote-generator/script.js
@@ -0,0 +1,19 @@
+let apiQuotes = []
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote);
+}
+
+async function getQuotes(){
+    const apiUrl = "https://type.fit/api/quotes"
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQuote()
+    } catch (error) {
+        
+    }
+}
+
+getQuotes()
