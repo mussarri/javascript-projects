74c84b2788d0f481b992590069c00f7f861d4b6e
musarri <msaricicek_99@hotmail.com>
2022-08-06 20:26:13 +0300

    functions added to buttons, progress bar made functional

---
+++ music-player/script.js
@@ -0,0 +1,83 @@
+import { songs } from "./songList.js"
+
+const music = document.querySelector("audio")
+const prevBtn = document.getElementById("prev")
+const playBtn = document.getElementById("play")
+const nextBtn = document.getElementById("next")
+const title = document.getElementById("title")
+const artist = document.getElementById("artist")
+const img = document.querySelector("img")
+const progressContainer = document.querySelector(".progress-container")
+const progress = document.querySelector(".progress")
+const currentTimeEl = document.getElementById("current-time")
+const durationEl = document.getElementById("duration")
+
+let isPlaying = false
+let currentIndex = 1
+
+loadSong()
+
+function play(){
+    isPlaying = true
+    playBtn.classList.replace("fa-play", "fa-pause")
+    music.play()
+}
+
+function pause(){
+    isPlaying = false
+    playBtn.classList.replace("fa-pause", "fa-play")
+    music.pause()
+}
+
+function loadSong(){
+    title.textContent = songs[currentIndex].title
+    artist.textContent = songs[currentIndex].artist
+    img.setAttribute("src", `${songs[currentIndex].imgSrc}`)
+    music.setAttribute("src", `${songs[currentIndex].audioUrl}`)
+}
+
+
+function updateProgressbar(e){
+    const secondNumber = (x) => x < 10 ? `0${x}` : x
+    const isTrue = (x) => x ? x : 0 
+
+    let {currentTime, duration} = e.target
+    progress.style.width = `${currentTime/duration * 100}%`
+
+    let minute = Math.floor(duration/60);
+    minute = isTrue(minute)
+    let second = Math.floor(duration%60);
+    second = secondNumber(second)
+    second = isTrue(second)
+    let currentMinute = Math.floor(currentTime/60);
+    currentMinute = isTrue(currentMinute)
+    let currentSecond = Math.floor(currentTime%60);
+    currentSecond = secondNumber(currentSecond)
+    currentSecond = isTrue(currentSecond)
+
+    durationEl.innerText = `${minute}:${second}`
+    currentTimeEl.innerText = `${currentMinute}:${currentSecond}`
+
+    e.target.volume = 1
+
+}
+
+
+playBtn.addEventListener("click", () => isPlaying ? pause() : play())
+nextBtn.addEventListener("click", () => {
+    currentIndex++
+    if(currentIndex > songs.length -1){
+        currentIndex = 0
+    }
+    loadSong()
+    isPlaying && music.play()
+})
+prevBtn.addEventListener("click", () => {
+    currentIndex--
+    if (currentIndex < 0) {
+        currentIndex = songs.length -1
+    }
+    loadSong()
+}
+)
+music.addEventListener("timeupdate", updateProgressbar)
---
+++ music-player/songList.js
@@ -0,0 +1,26 @@
+export const songs = [{
+    title:"Bir Kadın Çizeceksin",
+    artist:"maNga",
+    imgSrc: "https://upload.wikimedia.org/wikipedia/tr/thumb/d/d1/MaNga%2B.jpg/250px-MaNga%2B.jpg",
+    audioUrl : "https://www.mp3indirdur.info/mp3/indirdurArsiv333/maNga/Manga/maNga-Bir-Kadin-Cizeceksin.mp3"
+},{
+    title:"Beş",
+    artist:"Gripin",
+    imgSrc:"https://images.genius.com/a11da33be31998f53f0a0161cdaf0292.765x765x1.png",
+    audioUrl:"https://www.mp3indirdur.info/mp3/indirdurArsiv333/Gripin/M-S-05-03-2010/Gripin-Bes.mp3"
+},{
+    title:"Antidepresan Gülümsemesi",
+    artist:"Model",
+    imgSrc:"https://images.genius.com/f5db91caaf50e8a065cb02e44aac2277.640x640x1.jpg",
+    audioUrl:"https://www.mp3indirdur.info/mp3/indirdurArsiv333/Model/Levlanin-Hikayesi/Model-Antidepresan-Gulumsemesi-(Pazarlik).mp3"
+},{
+    title:"Believer",
+    artist:"Imagine Dragons",
+    imgSrc:"https://cdns-images.dzcdn.net/images/cover/7e8314f4280cffde363547a495a260bc/350x350.jpg",
+    audioUrl:"https://www.mp3indirdur.info/mp3/indirdurArsiv333/Imagine-Dragons/Evolve/Imagine-Dragons-Believer.mp3"
+},{
+    title:"Acının İlacı",
+    artist:"Adamlar",
+    imgSrc:"https://i.scdn.co/image/ab67616d00001e020b362fca4833c8d3b0cc5616",
+    audioUrl:"https://www.mp3indirdur.info/mp3/indirdurArsiv333/Adamlar/Ruyalarda-Burusmusum/Adamlar-Acinin-Ilaci.mp3"
+}]
